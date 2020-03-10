9416f9cdf47eb30ffed8e00002a31cac33f8cd95
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-10 18:38:26 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
   
   attr_accessor :board, :player_1, :player_2
   
-  def initialize(board = Board.new, player_1 = Players::Human, player_2 = Players::Human)
+  def initialize(board = Board.new, player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"))
     @board  = board 
     @board.display
     @player_1 = player_1
