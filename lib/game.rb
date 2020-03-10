15c0ab9323b38e8da71cb5c750c2a4d733e4bf18
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-10 18:41:50 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,12 @@ class Game
   
   attr_accessor :board, :player_1, :player_2
   
-  def initialize(board = Board.new, player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"))
-    @board  = board 
-    @board.display
+    def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
     @player_2 = player_2
-  end 
-  
-  
-  
+    @board = board
+    @board.display
+  end
   WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
