fea02e1e2af62f51ee4cc7993ad7dcd5a19e167f
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-11 06:15:22 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -24,11 +24,12 @@ class Game
   end 
   
   def won? 
-    WIN_COMBINATIONS.each do |win_combo| 
+    WIN_COMBINATIONS.each do |combination| 
        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
           @board.cells[combination[1]] == @board.cells[combination[2]] &&
           @board.taken?(combination[0]+1)
-      return win_combo
+      return combination
+    end 
   end 
   
 end 
