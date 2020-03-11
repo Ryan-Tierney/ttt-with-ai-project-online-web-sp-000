9266f91b830e07b811662c7ddf811df853930dcc
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-11 06:26:14 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,6 +61,8 @@ def turn
   puts "Please enter a number 1-9"
  @user_input = current_player.move(@board)
  if @board.valid_move?(@user_input)
-   @board.updatez(user_input, current_player)
+   @board.updatez(@user_input, current_player)
+ else 
+  puts ""
 end 
 end 
