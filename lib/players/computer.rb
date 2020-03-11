4c9f83c12cc98b83446583df34897bdeaaf57fcf
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-11 06:38:59 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,13 @@ module Players
     def move(board)
     if board.cells[4] == " "
       "5"
+    elsif board.cells[1] == " "
+    "0"
+    elsif board.cells[2] == " "
+    "1"
+    elsif board.cells[3] == " " 
+    "2" 
+    elsif board.cells[5] == 
     end 
   
   end 
